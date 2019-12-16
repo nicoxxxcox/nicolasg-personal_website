f659dd188ae44642dd7f6db08778b0a7c61d6d47
Nicoxcox <nicolas.grissi@gmail.com>
2019-12-16 17:19:39 +0100

    clean the js global context

--- app/js/_global.js
+++ app/js/_global.js
@@ -1,21 +1,31 @@
-// // Global
-// app.global = {
-//     init: function(){ // Load all global functions here
-//         console.log("load global functions");
-//         app.global.loadHeader();
-//     },
-//     loadHeader: function(){ // Some specific function
-//         console.log("loadHeader()");
-//     },
+ // // Instantiating the global app object
+ var app = {
+};
+ 
+ // Global
+ app.global = {
+     init: function(){ // Load all global functions here
+         console.log("load global functions");
+         app.global.loadHeader();
+         window.onload = app.global.displayBlogBtn();
+     },
+     loadHeader: function(){ // Some specific function
+       console.log("loadHeader()");
+     },
+     displayBlogBtn : function(){ // display blog button with delai
+        var btnBlog = document.getElementsByClassName('blogButton')[0];
+        btnBlog.classList.add('invisible');
+        window.setTimeout(function(){
+            btnBlog.classList.remove('invisible');
+            btnBlog.classList.add('fadeIn');
+           
+        },6000);
 
-//     displayBtn : function(){
+     }
+ }
 
-//         const btnBlog = document.getElementsByClassName('blogButton');
-//     }
-// }
-
-// // Run the global stuff
-// app.global.init();
+ // Run the global stuff
+ app.global.init();
 
 // //scripts
 
