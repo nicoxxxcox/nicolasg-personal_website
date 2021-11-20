04a8a157868546915385c8fb927150fccf450b56
nicolasg <nicolas.grissi@gmail.com>
2021-11-20 11:01:44 +0100

    layout : change mobile navigation layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,14 +44,6 @@
     })
   }
 
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
 
 
 })()
